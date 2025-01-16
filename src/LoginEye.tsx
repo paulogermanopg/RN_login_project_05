11b8abec33b53bb8d01b50abaafe9efec8402c3a
paulogermanopg <paulogermano.person@outlook.com.br>
2025-01-16 14:49:52 -0300

    feat: eye animation and title

--- src/LoginEye.tsx
+++ src/LoginEye.tsx
@@ -7,21 +7,25 @@ import Animated, {
 import Svg, {Path, Circle} from 'react-native-svg';
 import * as S from './styles';
 
+const AnimatedPath = Animated.createAnimatedComponent(Path);
 const AnimatedCircle = Animated.createAnimatedComponent(Circle);
 
 export default function LoginScreen() {
   const [email, setEmail] = useState('');
   const pupilX = useSharedValue(50);
   const pupilY = useSharedValue(30);
+  const eyeOpenHeight = useSharedValue(50);
 
   const handleEmailFocus = () => {
-    pupilX.value = withTiming(30);
+    const maxMovementX = 40;
+    const newX = 30 + Math.min(email.length * 1.5, maxMovementX);
+    pupilX.value = withTiming(newX);
   };
 
-  const handleEmailChange = text => {
+  const handleEmailChange = (text: string) => {
     setEmail(text);
     const maxMovementX = 40;
-    const newX = 30 + Math.min(text.length * 1.5, maxMovementX);
+    const newX = 30 + Math.min(text.length * 0.9, maxMovementX);
     pupilX.value = withTiming(newX);
 
     const baseY = 30;
@@ -29,7 +33,20 @@ export default function LoginScreen() {
     pupilY.value = withTiming(newY);
   };
 
-  // Propriedades animadas da pupila
+  const handlePasswordFocus = () => {
+    eyeOpenHeight.value = withTiming(0, {duration: 300});
+  };
+
+  const handlePasswordBlur = () => {
+    eyeOpenHeight.value = withTiming(50, {duration: 300});
+  };
+
+  const animatedEyeProps = useAnimatedProps(() => ({
+    d: `M5,25 Q50,${25 - eyeOpenHeight.value} 95,25 Q50,${
+      25 + eyeOpenHeight.value
+    } 5,25 Z`,
+  }));
+
   const animatedPupilProps = useAnimatedProps(() => ({
     cx: pupilX.value,
     cy: pupilY.value,
@@ -37,16 +54,21 @@ export default function LoginScreen() {
 
   return (
     <S.Container>
+      <S.TitleContainer>
+        <S.Title>LOGIN</S.Title>
+        <S.Title bigger>NIGHTMARES</S.Title>
+      </S.TitleContainer>
+
       <S.Eye>
-        <Svg width="100" height="50" viewBox="0 0 100 50">
-          <Path
-            d="M5,25 Q50,-10 95,25 Q50,60 5,25 Z"
+        <Svg width="100" height="60" viewBox="0 0 100 50">
+          <AnimatedPath
+            animatedProps={animatedEyeProps}
             fill="#fff"
             stroke="#000"
             strokeWidth="4"
           />
           <AnimatedCircle
-            r="10"
+            r="15"
             fill="black"
             animatedProps={animatedPupilProps}
           />
@@ -59,7 +81,12 @@ export default function LoginScreen() {
         onChangeText={handleEmailChange}
       />
 
-      <S.StyledInput placeholder="Senha" secureTextEntry />
+      <S.StyledInput
+        placeholder="Senha"
+        secureTextEntry
+        onFocus={handlePasswordFocus}
+        onBlur={handlePasswordBlur}
+      />
     </S.Container>
   );
 }
--- src/styles.ts
+++ src/styles.ts
@@ -1,10 +1,15 @@
 import styled from 'styled-components/native';
 import Animated from 'react-native-reanimated';
 
+type STYLEDPROPS = {
+  bigger?: boolean;
+};
+
 export const Container = styled.View`
   flex: 1;
   align-items: center;
   justify-content: center;
+  background-color: #000;
 `;
 
 export const Eye = styled.View`
@@ -24,4 +29,18 @@ export const StyledInput = styled.TextInput`
   padding: 10px;
   margin-vertical: 10px;
   border: 1px solid gray;
+`;
+
+export const TitleContainer = styled.View`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`;
+
+export const Title = styled.Text<STYLEDPROPS>`
+  font-size: ${(props: any) => props.bigger ? 50 : 20}px;
+  color: #fff;
+  font-family: AnnieUseYourTelescope-Regular;
+  letter-spacing: 2px;
+  margin-bottom: ${(props: any) => props.bigger ? 20 : -15}px;
 `;
